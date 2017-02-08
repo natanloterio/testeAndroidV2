1a2667d6419a997604d4bdcde6bd28121a622bf3
Natan Lotério <natanloterio@gmail.com>
2017-02-08 19:45:17 -0200

    Bugfix: DaoPessoaLocal estava inserindo nome no lugar do sobrenome no metodo getAll()

--- app/src/main/java/br/com/hbsis/testeandroidnatan/impl/dao/base/DaoPessoaLocal.java
+++ app/src/main/java/br/com/hbsis/testeandroidnatan/impl/dao/base/DaoPessoaLocal.java
@@ -24,6 +24,7 @@ public class DaoPessoaLocal implements IDao<Pessoa> {
     private String SELECT_SQL="Select * from pessoa";
 
     private final String COLUNA_ID = "id";
+    private final String COLUNA_ATIVO = "ativo";
     private final String COLUNA_NOME = "nome";
     private final String COLUNA_SOBRENOME = "sobrenome";
     private final String COLUNA_DATANASCIMENTO = "dataNascimento";
@@ -54,6 +55,17 @@ public class DaoPessoaLocal implements IDao<Pessoa> {
         return newRowId;
     }
 
+    public void setAtivo(Long id, boolean isAtivo) {
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUNA_ATIVO,isAtivo);
+
+        long newRowId = db.update(TABLE_NAME, values, "id="+id, null);
+        db.close();
+    }
+
     @Override
     public List<Pessoa> getAll() {
 
@@ -63,12 +75,15 @@ public class DaoPessoaLocal implements IDao<Pessoa> {
         List<Pessoa> pessoas = new ArrayList<>();
         if(c.moveToFirst()) {
             do {
-                String id = c.getString(0);
+                long id = c.getLong(0);
+                boolean ativo = c.getInt(c.getColumnIndex(COLUNA_ATIVO))>0;
                 String nome = c.getString(c.getColumnIndex(COLUNA_NOME));
                 String sobrenome = c.getString(c.getColumnIndex(COLUNA_SOBRENOME));
                 long longdataNascimenot = c.getLong(c.getColumnIndex(COLUNA_DATANASCIMENTO));
                 Date dataNascimento = new Date(longdataNascimenot);
                 Pessoa p = new Pessoa();
+                p.setId(id);
+                p.setAtivo(ativo);
                 p.setNome(nome);
                 p.setSobrenome(sobrenome);
                 p.setDataNascimento(dataNascimento);
@@ -83,4 +98,5 @@ public class DaoPessoaLocal implements IDao<Pessoa> {
     public SQLiteOpenHelper getNewDBHelper() {
         return new PessoaDBHelper(context);
     }
+
 }
